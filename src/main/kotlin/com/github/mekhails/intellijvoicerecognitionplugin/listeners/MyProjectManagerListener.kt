8e65f6d411a02c31dcfac793d357d82cf864894b
GitHub Action <action@github.com>
2022-02-26 09:40:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/mekhails/intellijvoicerecognitionplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/mekhails/intellijvoicerecognitionplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mekhails.intellijvoicerecognitionplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mekhails.intellijvoicerecognitionplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/mekhails/intellijvoicerecognitionplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mekhails.intellijvoicerecognitionplugin.services
+
+import com.github.mekhails.intellijvoicerecognitionplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
